4f83897ac53c8a400febff56a25a9eb3beb4ba6a
staka625 <takasum88@outlook.jp>
2023-10-28 08:08:51 +0900

    Revert "print debug"
    This reverts commit d0f705cd616d920184e9100aba3d10f16c55726c.

--- script.js
+++ script.js
@@ -35,10 +35,6 @@ async function selectFolder() {
     }
   }
 
-  console.log(sortedEntries);
-  console.log(jsonData);
-  console.log(imgEntity);
-
   if (jsonData) {
     setInnerHTMLById("album-title", jsonData.album.title);
     setInnerHTMLById("album-artist", jsonData.album.artist);
